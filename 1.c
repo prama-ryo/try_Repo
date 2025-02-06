4a5f7e33eaf97fb9bdae2c14b18d36de4c3bdc03
prama-ryo <pramod.rathod@matrixcomsec.com>
2025-02-06 15:13:29 +0530

    add summation file

--- 1.c
+++ 1.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include "subheader.h"
+#include "header.h"
 
 int main()
 {
 	int x=20;
 	int y=5;
+
+	int a = 10;
+	int b = 5;
 	printf("Hello new git hub \n");
-	printf("this is added line in this file");
+	printf("this is added line in this file\n");
 	printf("the sum of 5 + 3 is %d \n", 5+3);
 	int z= sub(x,y);
+	printf("This line is added from visual studio IDE 'editor' \n");
+	int c = sum(a,b);
 	return 0;
 }
---
+++ 2.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+int sum(int a, int b)
+{
+    printf("sum of a and b is:[%d]\n", a+b);
+}
