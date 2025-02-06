7cf1b771ed5189d59a2648ddd57de8603eb31b89
prama-ryo <pramod.rathod@matrixcomsec.com>
2025-02-06 15:09:29 +0530

    substraction file added

--- 1.c
+++ 1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include "subheader.h"
 
 int main()
 {
+	int x=20;
+	int y=5;
 	printf("Hello new git hub \n");
 	printf("this is added line in this file");
 	printf("the sum of 5 + 3 is %d \n", 5+3);
+	int z= sub(x,y);
 	return 0;
 }
---
+++ 3.c
@@ -0,0 +1,6 @@
+#include "subheader.h"
+
+int sub(int a, int b)
+{
+	printf("the substraction of a and b:[%d]\n", a-b);
+}
